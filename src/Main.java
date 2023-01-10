98582b9d0cabdc0aa4bb28f3a5c02076bb346db1
Anastasia <asiaa2110@gmail.com>
2023-01-10 16:01:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 7500;
+        int milesPrice = 20;
+        int miles = ticketPrice / milesPrice;
+        System.out.println((miles)+ " количество бонусных миль ");
+
+    }
+}
